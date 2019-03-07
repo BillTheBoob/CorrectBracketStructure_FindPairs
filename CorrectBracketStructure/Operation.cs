47fcc39428e00328f569b69022b4a13ab4153728
ipt.kuzz <ipt.kuzz@gmail.com>
2019-03-07 02:43:02 +0200

    first try

--- CorrectBracketStructure/Operation.cs
+++ CorrectBracketStructure/Operation.cs
@@ -8,6 +8,22 @@ namespace CorrectBracketStructure
 {
     class Operation
     {
-
+        public bool CheckupFunction(string s)
+        {
+            if (s[0] == ')' || s[s.Length - 1] == '(') { return false; }
+            int count = 0;
+            for(int i = 0; i< s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            return count != 0 ? false : true; 
+        }
     }
 }
--- CorrectBracketStructure/Test.cs
+++ CorrectBracketStructure/Test.cs
@@ -3,11 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace CorrectBracketStructure
 {
+    [TestFixture]
+
     class Test
     {
+        Operation operation = new Operation();
+
+        [Test]
+        [Description("Verifies that checkup function works correct.")]
+        [TestCase("()", true)]
+        [TestCase("(())", true)]
+        [TestCase("((((()))))", true)]
+        [TestCase("(((((((((((())))))))))))", true)]
+        [TestCase("(((()()()(())(())((((()))))())))", true)]
+        [TestCase("(((((()()(())((()()()()))()()())))))", true)]
+        [TestCase("(", false)]
+        [TestCase(")", false)]
+        [TestCase(")(", false)]
+        [TestCase(")()(", false)]
+        [TestCase("(()(()))", true)]
+        [TestCase("))()())((((())))(((((", false)]
+        [TestCase(")()(()()(()))())()())(()()()()())(()", false)]
+        [TestCase(")(()))(()())()))()())()))))))(((())())()()())", false)]
+        [TestCase("(((((((((((((((((((((((((((((((((((((((((((((", false)]
+        [TestCase("((()))((((()()()()()((((((((((((((()))))((()())))))))))))((((((()()()()()", false)]
+        [TestCase("(()()()())))(()()((()())))(((((((((((())))(()(()()))))(()()))))(())))))))))))))))()())()()", false)]
+        public void ValidityOfTheStringTest(string s, bool expected)
+        {
+            bool result = operation.CheckupFunction(s);
+            Assert.AreEqual(expected, result);
+        }
 
     }
 }
